f0be47820f3349f5533f389e2b06c00b5228acd2
Joy Lin <vvlnote@hotmail.com>
2019-10-02 00:20:12 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,7 +33,7 @@ def can_be_created_in_a_block(args = {:title =>"Home Alone", :release_date => 19
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
-
+  end
 end
 
 def can_get_the_first_item_in_the_database
